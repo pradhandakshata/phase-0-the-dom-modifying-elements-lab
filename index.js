073f40af783b9cbb283418c844b1fa24d08b9415
DakshataPradhan <pradhandakshata1996@gmail.com>
2022-06-20 19:24:15 -0600

    commit tree 1fc123d5db39b6b54c5a62e58d67adb56dbb8efa

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector("main#main").remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Dakshata is the champion!";
+newHeader.className = "victory";
